f805eecd0e2674ca03f8f3c965ddce57aa8c04a9
hai dang <141745582+haidangngo1234@users.noreply.github.com>
2024-07-25 20:24:40 +0700

    push

---
+++ main3.js
@@ -0,0 +1,19 @@
+// checkpoint 1
+
+// let m = [1, 2, 3, 4, 5, 6, "hh", "9", 80, 100];
+
+// let b = m.filter(item => typeof item === 'number');
+// let mSquared = m.map(item => Math.pow(item, 2));
+
+// console.log(m);
+
+// console.log(mSquared);
+
+//cau 2
+let s = " High knowledge, high return";
+
+let arr_s = s.split('').filter(item => item.trim() !=='').map(item => item.toLowerCase());
+console.log(s);
+
+
+
